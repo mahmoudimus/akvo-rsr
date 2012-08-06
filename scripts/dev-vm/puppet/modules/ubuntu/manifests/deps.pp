3d857fa8fcf65673db6b2c02b56147d1dafb1719
Daniel Karlsson <daniel@kardans.com>
2012-08-06 19:02:21 +0200

    Added PIL deps to dev env puppet scripts
    . #23

--- scripts/dev-vm/puppet/modules/ubuntu/manifests/deps.pp
+++ scripts/dev-vm/puppet/modules/ubuntu/manifests/deps.pp
@@ -8,6 +8,11 @@ class ubuntu::deps {
       "libxml2-dev",
       "ibxslt1-dev",
       "libmysqlclient-dev",
+      "build-dep",
+      "python-imaging",
+      "libjpeg62-dev",
+      "zlib1g-dev",
+      "libfreetype6-dev"
     ]:
     ensure => "latest",
     require => Class["ubuntu::bootstrap"],
