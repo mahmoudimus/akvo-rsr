d7d9aa8d5a22415084d6f64f2b1430c4532ffe64
Paul Burt <paul.burt@gmail.com>
2013-03-14 15:37:24 +0000

    Refactored test.akvo.rsr.middleware.test_partner_site_router_middleware using higher order pytest.mark.parametrize decorator.

--- tests/akvo/rsr/middleware/test_partner_site_router_middleware.py
+++ tests/akvo/rsr/middleware/test_partner_site_router_middleware.py
@@ -4,6 +4,8 @@
 # See more details in the license.txt file located at the root folder of the Akvo RSR module.
 # For additional details on the GNU license please see < http://www.gnu.org/licenses/agpl.html >.
 
+import pytest
+
 from akvo.rsr.middleware import (get_domain,
                                  is_partner_site_instance,
                                  is_rsr_instance)
@@ -15,7 +17,7 @@ __all__ = ["test_get_domain",
 
 
 class FakeRequest(object):
-    
+
     def __init__(self, host="www.akvo.org"):
         self.host = host
 
@@ -23,35 +25,28 @@ class FakeRequest(object):
         return self.host
 
 
-def pytest_generate_tests(metafunc):
-    hosts = (("localhost", "localhost"),
-             ("akvo.akvoapp.org", "akvo.akvoapp.org"),
-             ("www.akvo.org", "www.akvo.org"),
-             ("subsubdomain.subdomain.domain.com", "subdomain.domain.com"))
-    rsr_domains = ("akvo.org",
-                   "test.akvo.org",
-                   "www.akvo.org",
-                   "akvo.dev",
-                   "localhost")
-    ps_domains = ("akvo.akvotest3.org",
-                  "cordaid.akvoapp.org",
-                  "simavi.akvotest.org")
-    if "host" in metafunc.funcargnames:
-        metafunc.parametrize("host", hosts)
-    elif "rsr_domain" in metafunc.funcargnames:
-        metafunc.parametrize("rsr_domain", rsr_domains)
-    elif "ps_domain" in metafunc.funcargnames:
-        metafunc.parametrize("ps_domain", ps_domains)
+@pytest.mark.parametrize(("input", "expected"), (
+    ("localhost", "localhost"),
+    ("www.akvo.org", "www.akvo.org"),
+    ("akvo.akvoapp.org", "akvo.akvoapp.org"),
+    ("subsubdomain.subdomain.domain.com", "subdomain.domain.com")))
+def test_get_domain(input, expected):
+    assert get_domain(FakeRequest(host=input)) == expected
 
 
-def test_get_domain(host):
-    input_host, output_host = host
-    assert get_domain(FakeRequest(host=input_host)) == output_host
+@pytest.mark.parametrize("host", (
+    "akvo.org",
+    "test.akvo.org",
+    "www.akvo.org",
+    "akvo.dev",
+    "localhost"))
+def test_is_rsr_instance(host):
+    assert is_rsr_instance(host)
 
 
-def test_is_rsr_instance(rsr_domain):
-    assert is_rsr_instance(rsr_domain)
-
-
-def test_is_partner_site_instance(ps_domain):
-    assert is_partner_site_instance(ps_domain)
+@pytest.mark.parametrize("host", (
+    "akvo.akvotest3.org",
+    "cordaid.akvoapp.org",
+    "simavi.akvotest.org"))
+def test_is_partner_site_instance(host):
+    assert is_partner_site_instance(host)
